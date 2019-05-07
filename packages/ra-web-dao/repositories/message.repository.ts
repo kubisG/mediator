fea3fc501f20b7a7f9d8e3feed4530d8eaf9907b
vladimir-postulka <vladimir.postulka@rapidaddition.com>
2019-05-07 14:52:12 +0200

    RAOMS-175

--- packages/ra-web-dao/repositories/message.repository.ts
+++ packages/ra-web-dao/repositories/message.repository.ts
@@ -1,6 +1,7 @@
 import { RaMessage } from "@ra/web-core-be/db/entity/ra-message";
 import { EntityRepository,  Repository} from "typeorm";
 import { MessageType } from "@ra/web-core-be/enums/message-type.enum";
+import { OrdStatus } from "@ra/web-core-be/enums/ord-status.enum";
 
 @EntityRepository(RaMessage)
 export class MessageRepository extends  Repository<RaMessage> {
@@ -15,10 +16,12 @@ export class MessageRepository extends  Repository<RaMessage> {
             })
             .andWhere("not exists (select id from ra_message rm" +
                 " where rm.\"RaID\"=:raId and rm.\"app\"=:app and rm.\"ClOrdID\"=:OrigClOrdID and "
-                + "rm.\"msgType\" not in (:...BmsgType))"
+                + "((rm.\"msgType\" not in (:...BmsgType)) and (rm.\"msgType\" not in (:...CmsgType) and rm.\"OrdStatus\" not in (:...PendingNew)))"
                 , {
                     raId: raId, OrigClOrdID: OrigClOrdID, app: app,
-                    BmsgType: [MessageType.Replace, MessageType.Order, MessageType.Cancel]
+                    BmsgType: [MessageType.Replace, MessageType.Order, MessageType.Cancel],
+                    CmsgType: [MessageType.Execution],                    
+                    PendingNew: [OrdStatus.PendingNew]
                 })
             .getMany();
 
