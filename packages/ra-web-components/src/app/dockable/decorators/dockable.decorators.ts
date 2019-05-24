f3b8e3be285aebd0f73a417ceeb2aa654cc457e3
Hajek Jiri <hajekj14@gmail.com>
2019-05-24 12:07:37 +0200

    RAW-10

---
+++ packages/ra-web-components/src/app/dockable/decorators/dockable.decorators.ts
@@ -0,0 +1,10 @@
+import { Reflect } from "core-js";
+import { DockableConfig } from './dockable-config.interface';
+
+export const DOCKABLE_CONFIG: string = "__dockableConfig";
+
+export function Dockable(config: DockableConfig) {
+    return (target: any) => {
+        Reflect.defineMetadata(DOCKABLE_CONFIG, config, target);
+    };
+}
