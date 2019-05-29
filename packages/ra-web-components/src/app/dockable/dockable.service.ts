db41f7ea4e07e2a0d9ae50cf1a2cc92e25814017
Hajek Jiri <hajekj14@gmail.com>
2019-05-29 10:24:35 +0200

    RAW-15

--- packages/ra-web-components/src/app/dockable/dockable.service.ts
+++ packages/ra-web-components/src/app/dockable/dockable.service.ts
@@ -1,17 +1,34 @@
 import { Injectable, ElementRef } from "@angular/core";
+import { GoldenLayoutService } from "@embedded-enterprises/ng6-golden-layout";
 import { ComponentsMapService } from "./components-map.service";
 import { COMPONENT_ID } from "./constants";
+import { DockableComponentConfig } from "./dockable-component-config.interface";
 
 @Injectable()
 export class DockableService {
 
+    private singleComponentsMap: { [key: string]: DockableComponentConfig } = {};
+
     constructor(
         private componentsMapService: ComponentsMapService,
+        private goldenLayoutService: GoldenLayoutService,
     ) { }
 
     public stackBroadcast(msg: any, el: ElementRef) {
         const compId = el.nativeElement.getAttribute(COMPONENT_ID);
         this.componentsMapService.findOtherComponentsInStackAndEmitt(msg, compId);
+    }
+
+    public addComponent(config: DockableComponentConfig) {
+        if (config.single && this.singleComponentsMap[config.component.name]) {
+            return;
+        } else if (config.single && !this.singleComponentsMap[config.component.name]) {
+            this.singleComponentsMap[config.component.name] = config;
+        }
+        this.goldenLayoutService.createNewComponent({
+            componentName: config.componentName,
+            component: config.component
+        }, config.label);
     }
 
 }
