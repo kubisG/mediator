dd2329d31a78287045216cee9dda1cfb2c1e7dc0
Hajek Jiri <hajekj14@gmail.com>
2019-06-05 16:01:06 +0200

    RAW-15

--- packages/ra-web-components/src/app/dockable/dockable.service.ts
+++ packages/ra-web-components/src/app/dockable/dockable.service.ts
@@ -1,11 +1,12 @@
 import { Reflect } from "core-js";
 import { Injectable, ElementRef, Type } from "@angular/core";
-import { GoldenLayoutService } from "@embedded-enterprises/ng6-golden-layout";
+import { GoldenLayoutService, ComponentConfiguration } from "@embedded-enterprises/ng6-golden-layout";
 import { ComponentsMapService } from "./components-map.service";
 import { COMPONENT_ID } from "./constants";
 import { DockableComponentConfig } from "./dockable-component-config.interface";
 import { DOCKABLE_CONFIG } from "./decorators/dockable.decorators";
 import { DockableConfig } from "./decorators/dockable-config.interface";
+import * as GoldenLayout from "golden-layout";
 
 @Injectable()
 export class DockableService {
@@ -31,7 +32,7 @@ export class DockableService {
         this.goldenLayoutService.createNewComponent({
             componentName: config.componentName,
             component: config.component
-        }, config.label, config.state );
+        }, config.label, config.state);
     }
 
     public getComponentConfig(component: Type<any>): DockableConfig {
