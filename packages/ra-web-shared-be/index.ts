3d5e361401ee320e7ce2485a0663013f3ea106a7
Hajek Jiri <hajekj14@gmail.com>
2019-08-19 16:16:40 +0200

    RAW-55

--- packages/ra-web-shared-be/index.ts
+++
@@ -1,5 +0,0 @@
-export { CompaniesModule } from "./src/companies/companies.module";
-export { UsersModule } from "./src/users/users.module";
-export { PreferencesModule } from "./src/preferences/preferences.module";
-export { LayoutModule } from "./src/layout/layout.module";
-export { AppDirectoryModule } from "./src/app-directory/app-directory.module";
