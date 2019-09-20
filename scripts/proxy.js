ce4920387c279b0585263bc353c046c8d90fba6a
Hajek Jiri <hajekj14@gmail.com>
2019-09-20 13:32:45 +0200

    RAW-76

--- scripts/proxy.js
+++ scripts/proxy.js
@@ -5,7 +5,7 @@ var url = require('url');
 var proxy = httpProxy.createServer();
 
 http.createServer(function (req, res) {
-    console.log(`REQUEST: ${req.url}`)
+    console.log(`REQUEST: ${req.url}`);
     if (`${req.url}`.startsWith("/api/") || `${req.url}`.startsWith("/socket.io/")) {
         proxy.web(req, res, { target: 'http://localhost:3000' });
     } else {
