c5efe06b5f6063776109bba9141ccc9b8daa4174
Hajek Jiri <hajekj14@gmail.com>
2019-10-09 12:43:34 +0200

    RAW-77

--- packages/jest.config.js
+++ packages/jest.config.js
@@ -1,27 +1,68 @@
 const { defaults } = require("jest-config");
 
+// module.exports = {
+//     verbose: true,
+//     moduleFileExtensions: [
+//         "js",
+//         "json",
+//         "ts",
+//     ],
+//     coverageThreshold: {
+//         global: {
+//             "branches": 1,
+//             "functions": 1,
+//             "lines": 1,
+//             "statements": 1,
+//         },
+//     },
+//     rootDir: "./",
+//     testRegex: "(/tests/suites/.*|\\.(test|spec))\\.ts$",
+//     transform: {
+//         "^.+\\.(t|j)s$": "ts-jest",
+//     },
+//     coverageDirectory: "./coverage",
+//     collectCoverage: true,
+//     collectCoverageFrom: [
+//         "src/**/*.ts",
+//         "src/*.ts",
+//         "!src/**/*.d.ts",
+//         "!**/node_modules/**",
+//         "!src/**/*.provider.ts",
+//         "!src/**/*.providers.ts",
+//         "!src/**/*.module.ts",
+//         "!src/**/*.dto.ts",
+//         "!src/**/*.interface.ts",
+//         "!src/**/*.enum.ts",
+//         "!src/**/main.ts",
+//         "!src/**/main.hmr.ts",
+//     ],
+//     coverageReporters: [
+//         "json-summary",
+//         "lcov",
+//         "text",
+//     ],
+// };
+
 module.exports = {
-    verbose: true,
     moduleFileExtensions: [
         "js",
         "json",
         "ts",
     ],
-    coverageThreshold: {
-        global: {
-            "branches": 1,
-            "functions": 1,
-            "lines": 1,
-            "statements": 1,
-        },
+    transform: {
+        "^.+\\.(ts|html)$": "ts-jest",
+        "^.+\\.js$": "babel-jest"
     },
     rootDir: "./",
     testRegex: "(/tests/suites/.*|\\.(test|spec))\\.ts$",
-    transform: {
-        "^.+\\.(t|j)s$": "ts-jest",
-    },
     coverageDirectory: "./coverage",
-    collectCoverage: true,
+    coverageReporters: [
+        "json",
+        "json-summary",
+        "html",
+        "lcov",
+        "text"
+    ],
     collectCoverageFrom: [
         "src/**/*.ts",
         "src/*.ts",
@@ -36,9 +77,13 @@ module.exports = {
         "!src/**/main.ts",
         "!src/**/main.hmr.ts",
     ],
-    coverageReporters: [
-        "json-summary",
-        "lcov",
-        "text",
+    snapshotSerializers: [
+        "<rootDir>/../../node_modules/jest-preset-angular/AngularSnapshotSerializer.js",
+        "<rootDir>/../../node_modules/jest-preset-angular/HTMLCommentSerializer.js"
     ],
-};
+    modulePaths: [
+        "<rootDir>/../../dist",
+        "<rootDir>/../../node_modules"
+    ],
+    verbose: true
+}
