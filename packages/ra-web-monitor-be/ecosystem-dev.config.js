f2bd0a4fb2d09d61fca79e36760ac45652a5f61e
jiri-hajek <jiri.hajek@rapidaddition.com>
2019-11-07 18:31:53 +0100

    RAW-127 ecosystem change

--- packages/ra-web-monitor-be/ecosystem-dev.config.js
+++ packages/ra-web-monitor-be/ecosystem-dev.config.js
@@ -9,16 +9,6 @@ module.exports = {
                 "NODE_ENV": "dev",
                 "APP_MODE": "master",
             }
-        },
-        {
-            name: "ra-web-monitor-be-workers",
-            script: "./dist/main.js",
-            instances: 1,
-            exec_mode: "cluster",
-            env: {
-                "NODE_ENV": "dev",
-                "APP_MODE": "worker",
-            }
         }
     ]
 }
