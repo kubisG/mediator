369cedd49736e65e0929b097bca8cd50b22fe7c5
Jakub <jakub.gois@rapidaddition.com>
2019-11-19 19:50:46 +0100

    RAW-146: implement recursive and non recursive get files method

--- packages/ra-web-git/src/file/file.service.ts
+++ packages/ra-web-git/src/file/file.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from "@nestjs/common";
 import { FileDto } from "./dto/file.dto";
 import { ConfigService } from "src/config/config.service";
 
-import * as fs from "fs";
+import * as _fs from "fs";
 import * as _path from "path";
 
 @Injectable()
@@ -14,18 +14,36 @@ export class FileService {
         this.basePath = _path.resolve(configService.basePath);
     }
 
-    async getFiles(userName: string, repoKey: string, path: string, recursive?: boolean): Promise<FileDto[]> {
-        await this.getFilesRecursively(this.basePath);
-        return (recursive) ? [{} as FileDto, {} as FileDto] : [{} as FileDto];
+    async getFiles(userName: string, repoKey: string, relativeFilePath: string, recursive?: boolean): Promise<FileDto[]> {
+        const path: string = _path.join(this.basePath, userName, repoKey, relativeFilePath);
+        return (recursive) ? await this.getFilesByPathRecursively(path) : await this.getFilesByPath(path);
     }
 
-    async getFilesRecursively(path: string): Promise<FileDto[]> {
+    async getFilesByPath(path: string): Promise<FileDto[]> {
         const files: FileDto[] = [];
-        const dir = await fs.promises.opendir(path);
+        const dir = await _fs.promises.opendir(path);
+
         for await (const dirent of dir) {
-            if (dirent.isDirectory) {
+            const path: string = _path.join(dir.path, dirent.name);
+            const directory: boolean = dirent.isDirectory()
+            files.push({ name: dirent.name, path, directory });
+        }
+        return files;
+    }
+
+    async getFilesByPathRecursively(path: string): Promise<FileDto[]> {
+        const files: FileDto[] = [];
+        const dir = await _fs.promises.opendir(path);
+
+        for await (const dirent of dir) {
+            const path: string = _path.join(dir.path, dirent.name);
+            if (dirent.isDirectory()) {
+                const innerFiles: FileDto[] = await this.getFilesByPathRecursively(path);
+                files.push({ name: dirent.name, path, directory: true, files: innerFiles });
+            } else {
+                files.push({ name: dirent.name, path, directory: false });
             }
         }
-        return null;
+        return files;
     }
 }
