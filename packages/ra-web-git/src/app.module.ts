2437779a08ac5e09bd235b524f0bc63391523739
unknown <you@example.com>
2019-11-19 09:08:28 +0100

    RAW-146: initial commit, added git and file module and related classes, added dependency on simple-git

--- packages/ra-web-git/src/app.module.ts
+++ packages/ra-web-git/src/app.module.ts
@@ -5,6 +5,8 @@ import { ConfigModule } from "./config/config.module";
 import { AuthModule } from "@ra/web-auth-be/dist/auth.module";
 import { EnvironmentsModule } from "@ra/web-env-be/dist/environments.module";
 import { CoreModule } from "@ra/web-core-be/dist/core.module";
+import { FileModule } from "./file/file.module";
+import { GitModule } from "./git/git.module";
 
 @Module({
   imports: [
@@ -12,6 +14,8 @@ import { CoreModule } from "@ra/web-core-be/dist/core.module";
     CoreModule,
     EnvironmentsModule,
     ConfigModule,
+    FileModule,
+    GitModule,
   ],
   controllers: [AppController],
   providers: [AppService],
