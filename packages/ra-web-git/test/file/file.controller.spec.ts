8e9db346b120b4581e7bb052f294b3f7e5300ea0
Jakub <jakub.gois@rapidaddition.com>
2019-11-20 16:47:48 +0100

    RAW-146: fix controller dummy test

--- packages/ra-web-git/test/file/file.controller.spec.ts
+++ packages/ra-web-git/test/file/file.controller.spec.ts
@@ -1,11 +1,16 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { FileController } from "../../src/file/file.controller";
+import { FileService } from "../../src/file/file.service";
+
+// mock services
+jest.mock("../../src/file/file.service");
 
 describe("File Controller", () => {
   let controller: FileController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      providers: [FileService],
       controllers: [FileController],
     }).compile();
 
