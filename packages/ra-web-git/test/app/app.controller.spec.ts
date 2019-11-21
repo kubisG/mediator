73f2e730db1edde83bec3c85a3673ae41792f7b4
Jakub <jakub.gois@rapidaddition.com>
2019-11-21 12:40:24 +0100

    RAW-146: added first controller test with supertest, added service test

--- packages/ra-web-git/test/app/app.controller.spec.ts
+++ packages/ra-web-git/test/app/app.controller.spec.ts
@@ -1,31 +1,57 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { AppController } from "../../src/app.controller";
 import { AppService } from "../../src/app.service";
+import { HttpStatus } from "@nestjs/common";
+
+import * as request from "supertest";
 import * as pjson from "../../package.json";
 
+// mock services
+jest.mock("../../src/app.service");
+
 describe("AppController", () => {
-  let appController: AppController;
+  let app: any;
+  let requester: any;
   let appService: AppService;
 
+  // mock functions
+  let getInfoFn;
+
   beforeEach(async () => {
-    const app: TestingModule = await Test.createTestingModule({
+    const moduleFixture: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
       providers: [AppService],
     }).compile();
 
-    appController = app.get<AppController>(AppController);
-    appService = app.get<AppService>(AppService);
+    appService = moduleFixture.get<AppService>(AppService);
+    app = moduleFixture.createNestApplication();
+    // set up requester
+    requester = request(app.getHttpServer());
+    // run app to be able call endpoints
+    await app.init();
   });
 
-  it("should return application info", () => {
+  it("should return application info wihout any authorization", async (done) => {
     // prepare
-    const expectedInfo = { version: pjson.version, name: pjson.name };
-    jest.spyOn(appService, "getInfo").mockImplementation(() => expectedInfo);
+    const url = "/";
+    const expectedInfo = { version: pjson.version, name: "sdfa" };
+    getInfoFn = jest.spyOn(appService, "getInfo").mockReturnValue(expectedInfo);
 
     // execute
-    const info = appController.getInfo();
+    const response = await requester.get(url);
 
-    // verify
-    expect(info).toMatchObject(expectedInfo);
+    // verify method calls
+    expect(getInfoFn).toHaveBeenCalledTimes(1);
+    // verify result
+    expect(response.status).toBe(HttpStatus.OK); // always use HttpStatus over number value
+    expect(response.body).toMatchObject(expectedInfo); // user toMatchObject over toBe (serialization)
+
+    // call done when finish
+    done();
+  });
+
+  afterEach(() => {
+    // needs to be called after each test, otherwise it's accumulate calls from previous tests
+    getInfoFn.mockReset();
   });
 });
---
+++ packages/ra-web-git/test/app/app.service.spec.ts
@@ -0,0 +1,28 @@
+import { Test, TestingModule } from "@nestjs/testing";
+import { AppService } from "../../src/app.service";
+
+import * as _fs from "fs";
+import * as pjson from "../../package.json";
+
+describe("AppService", () => {
+  let appService: AppService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [AppService],
+    }).compile();
+
+    appService = module.get<AppService>(AppService);
+  });
+
+  it("should get list of files recursively", async () => {
+    // prepare input and results
+    const expectedInfo = { version: pjson.version, name: pjson.name };
+
+    // execute
+    const result = await appService.getInfo();
+
+    // verify result
+    expect(result).toMatchObject(expectedInfo);
+  });
+});
