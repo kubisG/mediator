392dc45802659786380f144ac60394556d25436e
Jakub <jakub.gois@rapidaddition.com>
2019-11-25 07:52:34 +0100

    RAW-149: reorder methods

--- packages/ra-web-git/src/file/file.service.ts
+++ packages/ra-web-git/src/file/file.service.ts
@@ -15,6 +15,70 @@ export class FileService {
         @Inject("logger") private logger: LoggerService,
     ) { }
 
+    private createPath(userName: string, repoKey: string, relativePath: string): string {
+        const basePath = _path.resolve(this.configService.basePath);
+        return _path.join(basePath, userName, repoKey, relativePath);
+    }
+
+    private getFileExtension(path: string): string {
+        const dotExtension: string = _path.extname(path);
+        return (dotExtension) ? dotExtension.split(".")[1] : null;
+    }
+
+    /**
+     * get files recursively
+     *
+     * Note: always try to avoid using 'fs.promises.opendir' cause it's hardly testable with mocks
+     *
+     * @param path
+     */
+    private async getFilesByPathRecursively(path: string): Promise<FileDto[]> {
+        const files: FileDto[] = [];
+
+        try {
+            const dirents: _fs.Dirent[] = await _fs.promises.readdir(path, { withFileTypes: true });
+
+            for (const dirent of dirents) {
+                const newPath: string = _path.join(path, dirent.name);
+                if (dirent.isDirectory()) {
+                    const innerFiles: FileDto[] = await this.getFilesByPathRecursively(newPath);
+                    files.push({ name: dirent.name, path: newPath, directory: true, files: innerFiles });
+                } else {
+                    files.push({ name: dirent.name, path: newPath, directory: false });
+                }
+            }
+        } catch (error) {
+            this.logger.error(error);
+            throw new InternalServerErrorException(`Fail during processing file: ${error.path}`);
+        }
+        return files;
+    }
+
+    /**
+     * get files
+     *
+     * Note: always try to avoid using 'fs.promises.opendir' cause it's hardly testable with mocks
+     *
+     * @param path
+     */
+    private async getFilesByPath(path: string): Promise<FileDto[]> {
+        const files: FileDto[] = [];
+
+        try {
+            const dirents: _fs.Dirent[] = await _fs.promises.readdir(path, { withFileTypes: true });
+
+            for (const dirent of dirents) {
+                const newPath: string = _path.join(path, dirent.name);
+                const directory: boolean = dirent.isDirectory();
+                files.push({ name: dirent.name, path: newPath, directory });
+            }
+        } catch (error) {
+            this.logger.error(error);
+            throw new InternalServerErrorException(`Fail during processing file: ${error.path}`);
+        }
+        return files;
+    }
+
     /**
      * method returns file content
      *
@@ -54,69 +118,5 @@ export class FileService {
     async getFiles(userName: string, repoKey: string, relativeFilePath: string, recursive?: boolean): Promise<FileDto[]> {
         const path: string = this.createPath(userName, repoKey, relativeFilePath);
         return (recursive) ? await this.getFilesByPathRecursively(path) : await this.getFilesByPath(path);
-    }
-
-    /**
-     * get files
-     *
-     * Note: always try to avoid using 'fs.promises.opendir' cause it's hardly testable with mocks
-     *
-     * @param path
-     */
-    private async getFilesByPath(path: string): Promise<FileDto[]> {
-        const files: FileDto[] = [];
-
-        try {
-            const dirents: _fs.Dirent[] = await _fs.promises.readdir(path, { withFileTypes: true });
-
-            for (const dirent of dirents) {
-                const newPath: string = _path.join(path, dirent.name);
-                const directory: boolean = dirent.isDirectory();
-                files.push({ name: dirent.name, path: newPath, directory });
-            }
-        } catch (error) {
-            this.logger.error(error);
-            throw new InternalServerErrorException(`Fail during processing file: ${error.path}`);
-        }
-        return files;
-    }
-
-    /**
-     * get files recursively
-     *
-     * Note: always try to avoid using 'fs.promises.opendir' cause it's hardly testable with mocks
-     *
-     * @param path
-     */
-    private async getFilesByPathRecursively(path: string): Promise<FileDto[]> {
-        const files: FileDto[] = [];
-
-        try {
-            const dirents: _fs.Dirent[] = await _fs.promises.readdir(path, { withFileTypes: true });
-
-            for (const dirent of dirents) {
-                const newPath: string = _path.join(path, dirent.name);
-                if (dirent.isDirectory()) {
-                    const innerFiles: FileDto[] = await this.getFilesByPathRecursively(newPath);
-                    files.push({ name: dirent.name, path: newPath, directory: true, files: innerFiles });
-                } else {
-                    files.push({ name: dirent.name, path: newPath, directory: false });
-                }
-            }
-        } catch (error) {
-            this.logger.error(error);
-            throw new InternalServerErrorException(`Fail during processing file: ${error.path}`);
-        }
-        return files;
-    }
-
-    private createPath(userName: string, repoKey: string, relativePath: string): string {
-        const basePath = _path.resolve(this.configService.basePath);
-        return _path.join(basePath, userName, repoKey, relativePath);
-    }
-
-    private getFileExtension(path: string): string {
-        const dotExtension: string = _path.extname(path);
-        return (dotExtension) ? dotExtension.split(".")[1] : null;
     }
 }
