2ca11590a3a93d2c9ea323ed61d328df74d9d784
Jakub <jakub.gois@rapidaddition.com>
2019-11-28 10:24:06 +0100

    RAW-143: added utils test, base git service tests

--- packages/ra-web-git/test/git/git.controller.spec.ts
+++ packages/ra-web-git/test/git/git.controller.spec.ts
@@ -1,5 +1,8 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { GitController } from "../../src/git/git.controller";
+import { GitService } from "../../src/git/git.service";
+
+jest.mock("../../src/git/git.service");
 
 describe("Git Controller", () => {
   let controller: GitController;
@@ -7,6 +10,7 @@ describe("Git Controller", () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [GitController],
+      providers: [GitService],
     }).compile();
 
     controller = module.get<GitController>(GitController);
--- packages/ra-web-git/test/git/git.service.spec.ts
+++ packages/ra-web-git/test/git/git.service.spec.ts
@@ -1,12 +1,24 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { GitService } from "../../src/git/git.service";
+import { ConfigService } from "../../src/config/config.service";
+import { Logger } from "@nestjs/common";
+
+jest.mock("../../src/config/config.service");
 
 describe("GitService", () => {
   let service: GitService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [GitService],
+      providers: [
+        GitService,
+        ConfigService,
+        {
+          provide: "logger",
+          useClass: Logger,
+        },
+
+      ],
     }).compile();
 
     service = module.get<GitService>(GitService);
